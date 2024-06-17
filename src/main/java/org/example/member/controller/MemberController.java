2d796fa0be5c3ced12a0ae2ff0136c4c16e6f246
YunChaeHyeon <44768255+YunChaeHyeon@users.noreply.github.com>
2024-06-17 14:48:02 +0900

    정규식 에러나서 수정

--- src/main/java/org/example/member/controller/MemberController.java
+++ src/main/java/org/example/member/controller/MemberController.java
@@ -39,7 +39,7 @@ public class MemberController {
         public String username;
         //        비밀번호
         @NotNull(message = "비밀번호는 6자리 이상 25자리 이하이며, 영어, 숫자, 특수문자를 포함해야 합니다.")
-        @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=[\\]{};':\"\\\\|,.<>/?]).{6,25}$")
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).{6,25}$")
         public String password;
         //        이름
         public String name;
