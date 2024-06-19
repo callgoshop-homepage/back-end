b50b654c59c4f68fe5b633b9eba2292c8b85bf41
gusrudchl12 <gusrudchl12@gmail.com>
2024-06-19 17:38:41 +0900

    멤버 승인처히

--- src/main/java/org/example/member/repository/MemberRepository.java
+++ src/main/java/org/example/member/repository/MemberRepository.java
@@ -9,4 +9,8 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUsername(String username);
+
+    Optional<Member> findByNameAndEmail(String name, String email);
+
+    Optional<Member> findByNameAndEmailAndUsername(String name, String email, String username);
 }
