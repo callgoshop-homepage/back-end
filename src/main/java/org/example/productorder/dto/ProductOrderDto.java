caa0c3c7ec63dc23e4d236b0a52d5859aa1800e3
gusrudchl12 <gusrudchl12@gmail.com>
2024-06-21 09:22:11 +0900

    엔티티 수정

--- src/main/java/org/example/productorder/dto/ProductOrderDto.java
+++ src/main/java/org/example/productorder/dto/ProductOrderDto.java
@@ -14,26 +14,14 @@ public class ProductOrderDto extends BaseEntity {
 
     private Long id;
 
-    //    주문 번호
-    private Long productOrderNumber;
-
     //    주문 상태
-    private boolean productOrderStatus;
+    private String productOrderStatus;
 
-    //    구매자 명
-    private String buyer;
-
-    //    구매자 전화번호
-    private String buyerPhoneNumber;
-
-    //    수취인 명
+    //    수취인명
     private String reciever;
 
     //    승인 처리일
     private LocalDateTime approvalDate;
-
-    //    발송 처리일
-    private LocalDateTime sendDate;
 
     //    배송 방법
     private String deliveryMethod;
@@ -43,21 +31,6 @@ public class ProductOrderDto extends BaseEntity {
 
     //    송장 번호
     private Long invoiceNumber;
-
-    //    상품 명
-    private String productName;
-
-    //    옵션 정보
-    private String optionInfo;
-
-    //    수량
-    private int productQuantity;
-
-    //    상품 가격
-    private Long productPrice;
-
-    //    옵션 가격
-    private Long productOptionPrice;
 
     //    담당자
     private String manager;
