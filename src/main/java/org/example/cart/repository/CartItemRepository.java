da28ed111d33bdcf0524d62874d69a416d0b5482
gusrudchl12 <gusrudchl12@gmail.com>
2024-06-24 11:19:04 +0900

    장바구니 불러오는 구문 수정

--- src/main/java/org/example/cart/repository/CartItemRepository.java
+++ src/main/java/org/example/cart/repository/CartItemRepository.java
@@ -1,9 +1,12 @@
 package org.example.cart.repository;
 
+import org.example.cart.entity.Cart;
 import org.example.cart.entity.CartItem;
+import org.example.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +14,6 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     CartItem findByCartIdAndProductId(Long cartId, Long productId);
 
     CartItem findByCartIdAndProductIdAndProductOptionId(Long id, Long id1, Long optionId);
+
+    List<CartItem> findByCart(Cart cart);
 }
