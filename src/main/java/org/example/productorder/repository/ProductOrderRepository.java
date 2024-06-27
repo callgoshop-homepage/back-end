02fa3c6f6893b9a2cc5399157c2732bdb1c8aa19
gusrudchl12 <gusrudchl12@gmail.com>
2024-06-27 14:55:45 +0900

    주문목록 수정

--- src/main/java/org/example/productorder/repository/ProductOrderRepository.java
+++ src/main/java/org/example/productorder/repository/ProductOrderRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 @Repository
 public interface ProductOrderRepository extends JpaRepository<ProductOrder, Long> {
     List<ProductOrder> findAll();
+    List<ProductOrder> findAllByOrderByIdDesc();
 }
